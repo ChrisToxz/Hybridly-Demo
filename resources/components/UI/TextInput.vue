b0b55e691c489b00fdfd47f08650a127b07d6193
ChrisToxz <chris@toxz.nl>
2023-09-24 19:41:22 +0200

    fix: TextInput component emit

--- resources/components/UI/TextInput.vue
+++ resources/components/UI/TextInput.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+
+defineEmits(['update:modelValue']);
 </script>
 
 <template>
   <input
   class="block w-full p-2 rounded-md shadow-sm bg-gray-700 border-gray-300 dark:border-gray-600 text-gray-200"
+  @input="$emit('update:modelValue', $event.target.value)"
   />
 </template>
