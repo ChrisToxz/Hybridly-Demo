6d5316da3b3f5b8aea9145f9d948ec72e98661ff
ChrisToxz <chris@toxz.nl>
2023-09-24 19:39:52 +0200

    Auth.ts composable for shared data

---
+++ resources/composables/auth.ts
@@ -0,0 +1,9 @@
+export default function useAuth() {
+    const user = useProperty('security.user')
+    const authenticated = computed(() => !!user.value)
+
+    return {
+        user,
+        authenticated,
+    }
+}
