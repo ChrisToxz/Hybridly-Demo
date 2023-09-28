daefa942ab067bfd8ba4ea4a86fae29d583a6681
ChrisToxz <chris@toxz.nl>
2023-09-28 21:01:55 +0200

    WIP Todo Card

---
+++ resources/views/welcome.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Button, Card, DropdownMenu, DropdownOption } from '@flavorly/vanilla-components'
+</script>
+
+<template layout>
+  <UITodoCard />
+  <br />
+  <!-- Regular -->
+  <div class="h-[220px]">
+    <Card
+      variant="todo"
+      title="Payment Methods"
+      subtitle="Here you can have your favorite payments methods"
+    >
+      <template #actions>
+        <DropdownMenu text="Options">
+          <DropdownOption>
+            <span>Copy Stuff</span>
+          </DropdownOption>
+          <DropdownOption>
+            <span>Mark as Done</span>
+          </DropdownOption>
+        </DropdownMenu>
+      </template>
+
+      <div class="p-5 text-sm">
+        <span>Some content on the card</span>
+      </div>
+
+      <template #footerActions>
+        <Button label="Resets" />
+        <Button
+          label="Submit"
+          variant="primary"
+        />
+      </template>
+    </Card>
+  </div>
+</template>
