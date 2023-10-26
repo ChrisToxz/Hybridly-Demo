a80f55a7feab2ab7e9b02fda39f895880d116f14
ChrisToxz <chris@toxz.nl>
2023-10-26 20:00:33 +0200

    Updated menu

---
+++ resources/components/UI/MenuItem.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <router-link class="text-xs font-semibold  tracking-wide uppercase hover:bg-gray-600/75 rounded-xl transition duration-300 py-2 px-4 hover:text-blue-400">
+    <slot />
+  </router-link>
+</template>
--- resources/layouts/default.vue
+++ resources/layouts/default.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import useAuth from '@/composables/auth'
+import MenuItem from '@/components/UI/MenuItem.vue'
 
 const {user, authenticated} = useAuth()
 
@@ -9,33 +10,24 @@ const {user, authenticated} = useAuth()
   <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
     <div class="container mx-auto">
       <nav class="p-4 flex justify-between">
-        <div class="text-lg font-medium flex-grow basis-0">
+        <div class="flex text-lg font-medium flex-grow basis-0">
           <template v-if="authenticated">
-            <router-link class="text-xs font-semibold uppercase tracking-wide">+ New Note</router-link>
+            <menu-item :href="route('index')">+ New note</menu-item>
           </template>
         </div>
         <div class="text-xl text-blue-600 font-bold text-center">
           <router-link :href="route('index')">CT Notes</router-link>
         </div>
-        <div v-if="authenticated" class="flex items-center gap-4 flex-grow basis-0 justify-end">
+        <div v-if="authenticated" class="flex items-center space-x-4 flex-grow basis-0 justify-end">
           <router-link class="text-sm text-gray-400">{{ user.name }}</router-link>
-          <router-link class="text-xs font-semibold uppercase tracking-wide" :href="route('index')">Notes</router-link>
-          <div>
-            <router-link :href="route('logout')" method="POST">
-              <div class="flex flex-row place-items-center transition duration-300 hover:text-white">
-                <!--                                <div class="grid h-12 w-12 place-items-center">-->
-                <!--                                    <i-material-symbols-logout-rounded class="mr-1.5 scale-[-1] text-2xl" />-->
-                <!--                                </div>-->
-                <div class="text-xs font-semibold uppercase tracking-wide">
-                  Logout
-                </div>
-              </div>
-            </router-link>
+          <div class="flex">
+            <menu-item :href="route('index')">Notes</menu-item>
+            <menu-item :href="route('logout')" method="POST">Logout</menu-item>
           </div>
         </div>
         <div v-else class="flex items-center gap-2 flex-grow basis-0 justify-end text-xs font-semibold uppercase tracking-wide">
-          <router-link class="text-gray-200" :href="route('register')">Register</router-link>
-          <router-link class="text-gray-200" :href="route('login')">Log in</router-link>
+          <menu-item :href="route('register')">Register</menu-item>
+          <menu-item :href="route('login')">Log in</menu-item>
         </div>
       </nav>
     </div>
