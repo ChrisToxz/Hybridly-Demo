2e6c390875ee3cf23116096383ff70c766db365c
ChrisToxz <chris@toxz.nl>
2023-10-27 21:14:45 +0200

    chore: Add AlpineJS

--- resources/application/main.ts
+++ resources/application/main.ts
@@ -1,31 +1,16 @@
 import { initializeHybridly } from 'virtual:hybridly/config'
-import {defineConfiguration, VanillaComponents} from '@flavorly/vanilla-components'
+import Alpine from 'alpinejs'
 import { createHead } from '@vueuse/head'
 import './tailwind.css'
 
+Alpine.start()
 
-
-// Define some overrides
-const overrides = {
-  Card: {
-    variants: {
-      todo: {
-        classes: {
-          card: 'relative bg-white shadow-lg rounded-lg dark:bg-gray-700/70 dark:text-white dark:shadow-dark-xl ring-1 ring-gray-900/10 dark:ring-white/10',
-          header: 'px-4 py-5 border-b border-gray-600 sm:px-6',
-          footer: 'px-4 py-3 flex items-center justify-center sm:justify-end text-right sm:px-6 border-t border-gray-600',
-        },
-      },
-    },
-  },
-}
 
 initializeHybridly({
   enhanceVue: (vue) => {
     vue.use(createHead({
       titleTemplate: (title) => title ? `${title} — Hybridly` : 'Hybridly',
-    })),
-    vue.use(VanillaComponents, defineConfiguration(overrides))
+    }))
   },
   progress: {
     color: '#3271a8',
