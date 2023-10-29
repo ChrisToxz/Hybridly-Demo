749474c2244676ac011ba1bc312194f32a146b38
ChrisToxz <chris@toxz.nl>
2023-10-29 18:01:48 +0100

    test: add Todo view tests

--- tests/Feature/Todo/ViewTest.php
+++ tests/Feature/Todo/ViewTest.php
@@ -17,14 +17,18 @@ it('can render todo view page', function () {
         ]);
 });
 
-//it('renders items on the todo view page', function () {
-//    $this->actingAs($this->user)
-//        ->get(route('todo.index'))
-//        ->assertOk()
-//        ->assertHybrid()
-//        ->assertHybridView('todo.index')
-//        ->assertHybridProperties([
-//            'items'
-//        ]);
-//
-//});
+it('loads todos and has the right properties', function () {
+
+    $todo = \App\Models\Todo::create(['title' => 'TestTitle', 'user_id' => $this->user->id]);
+
+    $response = $this->actingAs($this->user)
+        ->get(route('todo.index'))
+        ->assertOk()
+        ->assertHybrid()
+        ->assertHybridView('todo.index')
+        ->assertHybridProperties([
+            'todos'
+        ]);
+
+    expect($response->getHybridProperty('todos')[0])->toHaveKey('title', $todo->title);
+});
