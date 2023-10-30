e0538e07e742d896d9fdeb90e108ce5cb6cdca61
ChrisToxz <chris@toxz.nl>
2023-10-30 21:47:52 +0100

    Fix CreateTodoData

--- app/Data/CreateTodoData.php
+++ app/Data/CreateTodoData.php
@@ -8,7 +8,7 @@ class CreateTodoData extends Data
 {
     public function __construct(
         public readonly string $title,
-        public readonly ?string $description,
+        public readonly ?string $content,
     ) {
     }
 
