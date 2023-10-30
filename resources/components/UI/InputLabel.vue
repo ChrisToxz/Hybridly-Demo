732f0442a31acf5021492fa60e3e8b17bc9fff6b
ChrisToxz <chris@toxz.nl>
2023-10-30 20:33:41 +0100

    InputLabel Component fix

--- resources/components/UI/InputLabel.vue
+++ resources/components/UI/InputLabel.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-  <label class="block mb-1 text-gray-500 dark:text-gray-200 font-medium"><slot /></label>
+    <label class="block mb-1 text-gray-500 dark:text-gray-200 font-medium"><slot /></label>
 </template>
