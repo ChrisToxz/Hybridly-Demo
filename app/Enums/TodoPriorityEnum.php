4f4782b5f7a9b7d698cb35414e1e2807aa56bb80
ChrisToxz <chris@toxz.nl>
2023-10-31 22:39:30 +0100

    add Labels & Names for TodoPriorityEnum

--- app/Enums/TodoPriorityEnum.php
+++ app/Enums/TodoPriorityEnum.php
@@ -7,4 +7,17 @@ enum TodoPriorityEnum: string
     case Low = 'low';
     case Medium = 'medium';
     case High = 'high';
+
+    public function label(): string{
+        return match($this){
+            static::Low => 'Low priority',
+            static::Medium => 'Medium priority',
+            static::High => 'High priority'
+        };
+    }
+
+    public static function names(): array
+    {
+        return array_map(fn($enum) => $enum->name, static::cases());
+    }
 }
