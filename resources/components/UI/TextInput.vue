4fb25172a3f5ac81cf809586638f17152b679a1a
ChrisToxz <chris@toxz.nl>
2023-10-31 22:40:30 +0100

    Fix model binding in TextInput component

--- resources/components/UI/TextInput.vue
+++ resources/components/UI/TextInput.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 
-defineEmits(['update:modelValue']);
+// defineProps(['modelValue'])
+// defineEmits(['update:modelValue'])
+
+const modelValue = defineModel()
+
 </script>
 
 <template>
   <input
-  class="block w-full p-2 rounded-md shadow-sm bg-gray-700 border-gray-300 dark:border-gray-600 text-gray-200"
-  @input="$emit('update:modelValue', $event.target.value)"
+    v-model="modelValue"
+    class="block w-full p-2 rounded-md shadow-sm bg-gray-700 border-gray-300 dark:border-gray-600 text-gray-200"
   />
 </template>
