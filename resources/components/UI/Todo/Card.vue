f2c0c3d53715c098b294cd7c12b9f2039c715fbf
ChrisToxz <chris@toxz.nl>
2023-10-31 19:18:06 +0100

    Format created_at as timeAgo

--- resources/components/UI/Todo/Card.vue
+++ resources/components/UI/Todo/Card.vue
@@ -6,6 +6,7 @@ const $props = defineProps<{
   as: 'list-item'
 }>()
 
+const dynamicCreatedAt = useTimeAgo($props.todo.created_at)
 </script>
 
 <template>
@@ -20,11 +21,10 @@ const $props = defineProps<{
         <h1 class="mt-2 text-lg font-semibold text-gray-800 dark:text-white">{{ todo.title }}</h1>
         <p class="mt-2 text-sm text-gray-600 dark:text-gray-300">{{ todo.content }}</p>
       </div>
-
     </div>
 
     <div class="flex  items-center justify-between border-t border-gray-600 px-4 py-3 text-right mt-4">
-      <span class="text-sm font-light text-gray-800 dark:text-gray-400">{{ todo.created_at }}</span>
+      <span class="text-sm font-light text-gray-800 dark:text-gray-400">{{ dynamicCreatedAt }}</span>
       <div class="flex items-center space-x-2 text-right">
         <button as="button" class="focus:ring-primary-600 dark:focus:border-primary-600 block inline-flex cursor-pointer items-center justify-center whitespace-nowrap rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium text-gray-700 opacity-100 shadow focus:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:text-white dark:focus:text-white dark:focus:ring-offset-gray-900" type="button">
           <div class="flex items-center space-x-1"><span>Resets</span></div>
